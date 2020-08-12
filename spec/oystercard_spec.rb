c9df4a94784f79562267d8f95971869351ca810a
Djura22 <michaeliangroves@gmail.com>
2020-08-12 11:55:19 +0100

    oyster_spec moved from pairing over to master

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,42 +1,101 @@
-require 'oystercard'
-# recognise the oystercard file
-describe OysterCard do 
+require 'Oystercard'
 
-# Checking new card has 0 balance
-  it 'Balance of new card is zero' do
-  	expect(subject.balance).to eq(0)
+describe Oystercard do
+
+  it "Expect oystercard to respond to balance method" do
+    expect(subject).to respond_to(:balance)
   end
-# Checking that a new card instance can talk to a top-up method with a sgl arg.
-  it { is_expected.to respond_to(:top_up).with(1).argument }
 
-  it { is_expected.to respond_to(:deduct).with(1).argument }
+  it "Expects balance to be zero" do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq (0)
+  end
 
-  describe 'top_up' do
+  it "Expect oystercard to respond to top_up method" do
+    expect(subject).to respond_to(:top_up)
+  end
 
-    it 'top up to update balance of Oyster' do
-    	expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+  it "Expect oystercard to respond to top_up method with an amount" do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  it "Expect oystercard to respond to touch_in method with an argument" do
+    expect(subject).to respond_to(:touch_in).with(1).argument
+  end
+
+  describe "#top_up" do
+    it "Expects balance to increase by top up amount" do
+      amount = 10
+      expect{ subject.top_up amount }.to change{ subject.balance }.by 10
     end
 
-    it 'Balance should not exceed 90' do
-    	full_balance = OysterCard::LIMIT
-    	subject.top_up full_balance 
-    	expect { subject.top_up 1 }.to raise_error "Balance limit of #{full_balance} reached, try smaller amount"
+    it "Expects error if balance exceeds £90" do
+      amount = 100
+      expect{ subject.top_up amount }.to raise_error "The maximum limit is £90"
     end
   end
 
-  describe 'deduct' do
+  #describe "#deduct" do
+  #  it "Expext balance to decrease by deduct amount" do
+  #    amount = 10
+  #    expect{ subject.deduct amount }.to change{ subject.balance }.by -10
+  #  end
+  #end
 
-  	it 'deduct from balance' do
-  	  subject.top_up(1)
-  		expect{ subject.deduct 1 }.to change{ subject.balance }.by -1
-  	end
+  describe '#touch_in' do
 
-  	it 'prevents use of card at 0 balance' do
-  		card = OysterCard.new
-  		subject.top_up(1)
-  		expect { card.deduct 1 }.to raise_error "Time to top up! Balance: 0"
-  	end
+    let(:station){ double :station }
+
+    context 'card has been touched in with valid balance' do
+      before do
+        subject.top_up(Oystercard::MAX_BALANCE)
+      end
+
+      it 'change status of oystercard in_journey? = true' do
+        expect{ subject.touch_in(station) }.to change{ subject.in_journey }.to true
+      end
+    end
+
+      it "Expects an error for touching in if balance is less than £1" do
+        expect{ subject.touch_in(station) }.to raise_error "You need to top up"
+      end
+
+      it "Stores the entry station" do
+        subject.top_up(Oystercard::MAX_BALANCE)
+        subject.touch_in(station)
+        expect(subject.entry_station).to eq station
+      end
   end
+
+  describe '#touch_out' do
+
+  let(:station){ double :station }
+
+    it 'change status of oystercard in_journey? = false' do
+      subject.top_up(Oystercard::MAX_BALANCE)
+      subject.touch_in(station)
+      expect{ subject.touch_out }.to change{ subject.in_journey }.to false
+    end
+
+    context 'balance is reduced by MIN_BALANCE on touch_out' do
+      before do
+        subject.top_up(Oystercard::MAX_BALANCE)
+      end
+
+      it 'reduces balance by MIN_BALANCE' do
+        expect{ subject.touch_out }.to change{ subject.balance }.by -1
+      end
+    end
+
+
+  end
+
+  describe '#in_journey' do
+    it "Expect in_journey to equal to false" do
+      expect(subject.in_journey).to eq false
+    end
+  end
+
+
 
 end
-
