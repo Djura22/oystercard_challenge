9de01eec372319cae9df23669dbe09f848a0fbca
Djura22 <michaeliangroves@gmail.com>
2020-08-12 11:56:16 +0100

    oystercard.rb updated from pairing session

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,28 +1,46 @@
-# Oystercard class created
-class OysterCard
-# Card limit set 
-	LIMIT = 90
-# balance method refactored into attr_reader
+class Oystercard
+
   attr_reader :balance
-# initializing a balance on the new card. Set at 0
+  attr_reader :entry_station
+  attr_accessor :in_journey
+
+
+  MAX_BALANCE = 90
+  MIN_BALANCE = 1
+
   def initialize
-  	@balance = 0
-  	#@limit = LIMIT
-  end
-# method for topping up the owners card balance
-  def top_up(amount)
-  	fail "Balance limit of #{LIMIT} reached, try smaller amount" if @balance + amount > LIMIT
-  	@balance += amount
+    @balance = 0
+    @in_journey = false
+    @entry_station = nil
   end
 
+  def top_up(amount)
+    fail "The maximum limit is £90" if @balance + amount > MAX_BALANCE
+    @balance += amount
+  end
+
+  def touch_in(station)
+    fail "You need to top up" if @balance < MIN_BALANCE
+    @in_journey = true
+    @entry_station = station
+
+  end
+
+  def touch_out
+    deduct(MIN_BALANCE)
+    @in_journey = false
+    @entry_station = nil
+  end
+
+  def in_journey?
+    !!entry_station
+  end
+
+  private
+
   def deduct(amount)
-  	fail "Time to top up! Balance: 0" if @balance == 0
     @balance -= amount
   end
 
 
-
-
-
-	
 end
